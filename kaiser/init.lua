eb9d1e2c7f166ccbbd6cec8e4c6392e7bff0ef54
Kay <kay.kostadinov@gmail.com>
2023-10-17 11:32:42 +0300

    initial commit

---
+++ kaiser/init.lua
@@ -0,0 +1,19 @@
+vim.g.mapleader = " "
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+
+vim.opt.termguicolors = true
+vim.opt.incsearch = true
+vim.opt.scrolloff = 10
+vim.opt.updatetime = 50
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
